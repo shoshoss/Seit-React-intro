7b1d80a4497e67abf252b44ad1fd8e78c9596e3d
shota <s8152.45@gmail.com>
2025-02-20 11:28:14 +0900

    Add: ボタンのスタイルの当て方

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,18 @@
+import styles from "./Button.module.css";
+
 const Button = ({ disable, type, children }) => {
 	const handleClick = () => {
 		alert("Button clicked!");
 	};
 
 	return (
-		<button disabled={disable} type={type} onClick={() => handleClick()}>
-			{children}
+		<button
+			className={styles.button}
+			disabled={disable}
+			type={type}
+			onClick={() => handleClick()}
+		>
+			<span className={styles.buttonInner}>{children}</span>
 		</button>
 	);
 };
