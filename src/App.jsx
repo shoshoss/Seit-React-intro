8a067567489b898a7ef3696a29dee7f8eaf43397
shota <s8152.45@gmail.com>
2025-02-25 13:29:58 +0900

    Add: 初期値をLoadingとして1秒後にCountに表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Display from "./components/Display/Display";
-import { useEffect } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	useEffect(() => {
-		console.log("counted: +", count);
-	}, [count]);
-
 	return (
 		<>
-			<Display count={count}>Count</Display>
+			<Display count={count} />
 			<br />
 			<Button disable={false} type="button" onClick={() => setCount(count + 1)}>
 				<span>ボタン</span>
